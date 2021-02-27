7608d2117dd72c523d594bd41b9feb58f9f969eb
SamplerDev <74883277+SamplerDev@users.noreply.github.com>
2021-02-27 19:08:54 -0300

    Buscador y carrito

--- js/index.js
+++ js/index.js
@@ -50,3 +50,65 @@ links.map(link => {
     document.body.classList.remove("show");
   });
 });
+
+/**Buscador */
+
+const formulario = document.querySelector('#formulario');
+         const boton= document.querySelector('#boton');
+         const contenedor1 = document.getElementsByClassName('contenedor1');
+         const resultado = document.querySelector('#resultado');
+        
+         const tiendas= [
+          {nombre:'Turismo',
+
+          nombre2:'Sol y Luna',link: `<a href="SolyLuna.html">Turismo Sol y Luna</a>` },
+
+          {nombre:'Turismo',
+
+          nombre2:'ejemplo',link: `<a href="SolyLuna.html">Ejemplo</a>` }
+
+
+         ]
+         const filtrar = () =>{
+
+          resultado.innerHTML ='';
+           const texto = formulario.value.toLowerCase();
+          
+
+           
+           for(let tienda of tiendas){
+             let nombre = tienda.nombre.toLowerCase();
+             let nombre2 = tienda.nombre2.toLocaleLowerCase();
+             if ( nombre == texto || nombre2 == texto){
+                   resultado.innerHTML +=`
+
+
+                   <li id="resultados" > ${tienda.link} </li>
+                   
+                   `
+
+             }
+
+             
+
+             
+           }
+           
+
+          
+
+           
+           
+             
+          
+          
+          
+          }
+           
+           boton.addEventListener('click',filtrar); 
+
+
+             
+            
+
+ 
